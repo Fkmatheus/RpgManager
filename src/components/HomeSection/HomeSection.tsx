227c2ff773587e7cb520e351201d4bcb09a83da0
Matheus Santos D S <127159629+Fkmatheus@users.noreply.github.com>
2026-03-16 11:41:49 -0300

    feat: Inicio do esqueleto de HomeSection

--- src/components/HomeSection/HomeSection.tsx
+++ src/components/HomeSection/HomeSection.tsx
@@ -1,12 +1,46 @@
-import { Box, Flex } from "@chakra-ui/react";
-
+import { Box, Flex, Text } from "@chakra-ui/react";
 
 export function HomeSection() {
-    return (
-        <>
-            <Flex height={"100vh"} width={"100%"} backgroundColor={"gray.900"} justifyContent={"center"}>
-                <p>teste</p>
-            </Flex>
-        </>
-    )
+  return (
+    <>
+      <Flex
+        height={"100vh"}
+        width={"100%"}
+        backgroundColor={"gray.900"}
+        justifyContent={"center"}
+        marginLeft={"15%"}
+      >
+        <Flex width={"95%"} justifyContent={"center"}>
+          <Box display={"flex"} flexDirection={"row"} width={"90%"} justifyContent={"center"}>
+            <Box
+              marginTop={3}
+              border={"solid 1px black"}
+              width={"60%"}
+              height={"80vh"}
+            ></Box>
+            <Box
+              marginTop={3}
+              marginLeft={5}
+              display={"flex"}
+              justifyContent={"space-between"}
+              flexDirection={"column"}
+              width={"40%"}
+              height={"80vh"}
+            >
+              <Box
+                border={"solid 1px black"}
+                height={"38vh"}
+                width={"100%"}
+              ></Box>
+              <Box
+                border={"solid 1px black"}
+                height={"38vh"}
+                width={"100%"}
+              ></Box>
+            </Box>
+          </Box>
+        </Flex>
+      </Flex>
+    </>
+  );
 }
