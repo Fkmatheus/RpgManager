d5beb7b3e2e2fa084c6f99a213f2b7f3d3e5a626
Matheus Santos D S <fkmatheus.profissional@gmail.com>
2026-03-16 22:02:07 -0300

    feat: Preenchendo algumas sessões de Home com imagens

--- src/components/HomeSection/HomeSection.tsx
+++ src/components/HomeSection/HomeSection.tsx
@@ -1,4 +1,7 @@
 import { Box, Flex, Text } from "@chakra-ui/react";
+import logoArtTesouro from "../../img/art.png";
+import logoArtMagicos from "../../img/art2.png";
+import logoArtGemas from "../../img/art3.png";
 
 export function HomeSection() {
   return (
@@ -10,14 +13,26 @@ export function HomeSection() {
         justifyContent={"center"}
         marginLeft={"15%"}
       >
-        <Flex width={"95%"} justifyContent={"center"}>
+        <Flex width={"100%"} justifyContent={"center"}>
           <Box display={"flex"} flexDirection={"row"} width={"90%"} justifyContent={"center"}>
             <Box
               marginTop={3}
               border={"solid 1px black"}
               width={"60%"}
-              height={"80vh"}
-            ></Box>
+              height={"95vh"}
+              bgImage={`linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(${logoArtTesouro})`}
+              bgSize={"cover"}
+              bgPosition={"center"}
+              bgRepeat={"no-repeat"}
+              textAlign={"center"}
+              display={"flex"}
+              flexDirection={"column"}
+              alignItems={"flex-start"}
+              justifyContent={"center"}
+            >
+              <Text fontWeight={"bold"} marginTop={"45%"} marginLeft={5} fontFamily={'Cinzel'} fontSize={40} color={"white"}>Objetos de arte</Text>
+              <Text marginTop={"2.5%"} textAlign={"left"} marginLeft={5} fontFamily={'Cinzel'} fontSize={20} color={"gray.300"}>Role seus diferentes tipos possiveis de objetos de arte valiosos. As possibilidades são extremamente variadas, desde a um simples Jarro de prata até um colar de diamantes.</Text>
+            </Box>
             <Box
               marginTop={3}
               marginLeft={5}
@@ -25,18 +40,42 @@ export function HomeSection() {
               justifyContent={"space-between"}
               flexDirection={"column"}
               width={"40%"}
-              height={"80vh"}
+              height={"95vh"}
             >
               <Box
                 border={"solid 1px black"}
-                height={"38vh"}
+                height={"46vh"}
                 width={"100%"}
-              ></Box>
+                bgImage={`linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(${logoArtMagicos})`}
+                bgSize={"cover"}
+                bgPosition={"center"}
+                bgRepeat={"no-repeat"}
+                textAlign={"center"}
+                display={"flex"}
+                flexDirection={"column"}
+                alignItems={"flex-start"}
+                justifyContent={"center"}
+              >
+                <Text fontWeight={"bold"} marginTop={"30%"} marginLeft={5} fontFamily={'Cinzel'} fontSize={30} color={"white"}>Itens mágicos</Text>
+                <Text marginTop={"2.5%"} textAlign={"left"} marginLeft={5} fontFamily={'Cinzel'} fontSize={15} color={"gray.300"}>Role seus diferentes tipos possiveis de itens mágicos, dê aquele upgrade super importante em seu personagem e deixe seu mestre de cabelos arrepiados.</Text>
+              </Box>
               <Box
                 border={"solid 1px black"}
-                height={"38vh"}
+                height={"46vh"}
                 width={"100%"}
-              ></Box>
+                bgImage={`linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(${logoArtGemas})`}
+                bgSize={"cover"}
+                bgPosition={"center"}
+                bgRepeat={"no-repeat"}
+                textAlign={"center"}
+                display={"flex"}
+                flexDirection={"column"}
+                alignItems={"flex-start"}
+                justifyContent={"center"}
+              >
+                <Text fontWeight={"bold"} marginTop={"30%"} marginLeft={5} fontFamily={'Cinzel'} fontSize={30} color={"white"}>Gemas</Text>
+                <Text marginTop={"2.5%"} textAlign={"left"} marginLeft={5} fontFamily={'Cinzel'} fontSize={15} color={"gray.300"}>Role seus diferentes tipos possiveis de Gemas, consiga muito ouro vendendo ou simplesmente utilize elas e fique radiante!.</Text>
+              </Box>
             </Box>
           </Box>
         </Flex>
