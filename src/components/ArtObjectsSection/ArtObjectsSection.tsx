9d5a142d40a28fbe973d8ede40b60ceb5155b7b4
Matheus Santos D S <fkmatheus.profissional@gmail.com>
2026-03-20 21:04:05 -0300

    feat: Pequenos ajustes em ArtObjectsSection

--- src/components/ArtObjectsSection/ArtObjectsSection.tsx
+++ src/components/ArtObjectsSection/ArtObjectsSection.tsx
@@ -1,11 +1,11 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Text } from '@chakra-ui/react';
 import { Footer } from "../Footer/Footer";
 
 export function ArtObjectsSection() {
   return (
     <>
       <Flex
-        height={"200vh"}
+        height={"300vh"}
         width={"100%"}
         backgroundColor={"gray.900"}
         flexDirection={"column"}
@@ -20,7 +20,12 @@ export function ArtObjectsSection() {
           flexDirection={"row"}
           justifyContent={"space-between"}
         >
-          <Box width={"48%"} height={"60vh"} border={"solid 1px black"}></Box>
+          <Box width={"48%"} height={"60vh"} border={"solid 1px black"}>
+            <Box display={"flex"} flexDirection={"row"} fontSize={20} alignItems={"center"} textAlign={"center"} fontWeight={"bold"} fontFamily={'Cinzel'} color={"white"} width={"100%"} height={"6vh"} backgroundColor={"#111111"} justifyContent={"space-between"}>
+               <Text marginLeft={3}>Objetos de arte [25po]</Text>
+               <Text>TESTE 2</Text>
+            </Box>
+          </Box>
           <Box width={"48%"} height={"60vh"} border={"solid 1px black"}></Box>
         </Box>
         <Box
@@ -35,10 +40,16 @@ export function ArtObjectsSection() {
           <Box width={"48%"} height={"60vh"} border={"solid 1px black"}></Box>
           <Box width={"48%"} height={"60vh"} border={"solid 1px black"}></Box>
         </Box>
-        <Box>
-          <Box></Box>
+        <Box
+          marginTop={"5%"}
+          marginLeft={"10%"}
+          width={"85%"}
+          height={"70vh"}
+          border={"solid 1px black"}>
+
+
         </Box>
-        <Footer/>
+        <Footer />
       </Flex>
     </>
   );
