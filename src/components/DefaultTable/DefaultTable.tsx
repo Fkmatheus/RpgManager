61ffa089635e580622309ca837a3b2a971f55aec
Matheus Santos D S <127159629+Fkmatheus@users.noreply.github.com>
2026-03-25 12:34:33 -0300

    feat: Deixando o modal de DefaultTable genérico

--- src/components/DefaultTable/DefaultTable.tsx
+++ src/components/DefaultTable/DefaultTable.tsx
@@ -6,20 +6,10 @@ import {
   Th,
   Td,
   TableContainer,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalCloseButton,
-  ModalBody,
-  ModalFooter,
-  Button,
   useDisclosure,
-  Text,
-  Box
 } from "@chakra-ui/react";
-import { useState } from "react";
-import imgPocaoGigante from "../../img/pocaoGigante.png";
+import { useState } from "react"
+import { DefaultModal } from "../DefaultModal/DefaultModal";
 
 interface TableItem {
   id: number;
@@ -70,43 +60,7 @@ export function DefaultTable({ dice, type, data, fontSize }: DefaultTableProps) 
         </Table>
       </TableContainer>
 
-      <Modal isOpen={isOpen} onClose={onClose} size="xl" isCentered>
-        <ModalOverlay />
-        <ModalContent fontFamily={'Cinzel'}>
-          <ModalHeader color={"white"} backgroundColor={"#111111"}>{itemName}</ModalHeader>
-          <ModalCloseButton />
-
-          <ModalBody fontSize={14} fontFamily={'Cinzel'} backgroundColor={"#161616"} color={"gray.300"}>
-            <Box width={"100%"} display={"flex"} flexDirection={"row"} justifyContent={"space-between"}>
-              <Box width={"46%"} border={"solid 1px black"} bgImage={`linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url(${imgPocaoGigante})`}
-                bgSize={"cover"}
-                bgPosition={"center"}
-                bgRepeat={"no-repeat"}></Box>
-              <Box width={"46%"} height={"40vh"} overflowY="auto"
-                sx={{
-                  "&::-webkit-scrollbar": {
-                    display: "none",
-                  },
-                  msOverflowStyle: "none",
-                  scrollbarWidth: "none",
-                }}> <Text>Quando bebe esta poção, seu valor de Força muda por 1
-                  hora. O tipo de gigante determinar o valor (veja na tabela
-                  abaixo). A poção não produz efeito em você caso sua Força
-                  seja igual ou maior que o valor dela.
-                  O líquido transparente dessa poção tem um pedaço de
-                  unha de um gigante do tipo apropriado flutuando nele. A
-                  poção de força do gigante do gelo e a poção de força do
-                  gigante de pedra têm o mesmo efeito.</Text>  </Box>
-            </Box>
-          </ModalBody>
-
-          <ModalFooter backgroundColor={"#161616"}>
-            <Button _hover={{ fontWeight: "bold", color: "tomato" }} variant="ghost" mr={3} onClick={onClose} fontWeight={"bold"} color={"white"}>
-              Fechar
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+      <DefaultModal isOpen={isOpen} itemName={itemName} onClose={onClose}/>
     </>
   );
 }
