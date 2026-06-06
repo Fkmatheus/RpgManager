28ceb908c54b031b655d111369d6e01695f52bea
Matheus Santos D S <fkmatheus.profissional@gmail.com>
2026-06-06 20:40:24 -0300

    fix: Ajustando o axios para interagir com o tailscale

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:45000",
+  // baseURL: "http://localhost:45000",
+  baseURL: "http://100.107.127.78:45000",
 });
